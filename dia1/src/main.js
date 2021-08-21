e4d02e9d522c51ac18a1dfea10aec6a30af0cf1c
Matheus <matheushenrique4@hotmail.com>
2021-08-21 00:44:03 -0300

    #4 - Criando o botão para alternar a visibilidade da div

--- dia1/src/main.js
+++ dia1/src/main.js
@@ -4,3 +4,22 @@ document.querySelector('[data-js=app]').innerHTML = `
   <h1>B. Academy</h1>
   <p>Boas vindas à semana de pré-work para o Bootcamp em React.js 😁</p>
 `
+const buttonVisibility = document.querySelector('[data-js=switch-visibility]')
+
+buttonVisibility.addEventListener('click', (event) => {
+  event.preventDefault()
+
+  const div = document.querySelectorAll('[data-js=app]')
+  const status = {
+    true: () => {
+      div[0].hidden = false
+      buttonVisibility.innerHTML = "Esconder"
+    },
+    false: () => {
+      div[0].hidden = true
+      buttonVisibility.innerHTML = "Mostrar"
+    }
+  }
+
+  status[div[0].hidden]()
+}, false)
