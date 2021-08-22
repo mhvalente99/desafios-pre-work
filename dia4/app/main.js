52dc81e27225805d8d0cd3158a42cf3b5dbba032
Matheus <matheushenrique4@hotmail.com>
2021-08-22 16:44:20 -0300

    #2 - Fazendo o tratramento de retorno vazio, apos cadastrar o carro sera tratado para preencher os dados na tabela

--- dia4/app/main.js
+++ dia4/app/main.js
@@ -1,1 +1,25 @@
 import './style.css'
+
+const url = 'http://localhost:3333/cars'
+
+const table = document.querySelector('[data-js=table]')
+
+function montarCarrosVazio() {
+  const linha = document.createElement('tr')
+  const coluna = document.createElement('td')
+
+  coluna.textContent = 'Nenhum carro encontrado'
+
+  linha.appendChild(coluna)
+  table.appendChild(linha)
+}
+
+async function listarCarros() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      data.length === 0 ? montarCarrosVazio() : data
+    })
+}
+
+listarCarros()
