03b0077567812b0eea0beef0d141c7265526a6df
Matheus <matheushenrique4@hotmail.com>
2021-08-22 12:06:06 -0300

    #2 - Criando as options por um objeto e listando as cores selecionadas em uma div nova

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -8,3 +8,60 @@ nome.addEventListener('input', (e) => {
   e.target.value = trataNome(e.target.value)
   console.log(e.target.value);
 })
+
+const cor = [
+  {
+    nome: 'Verde',
+    rgb: '#00FF00'
+  },
+  {
+    nome: 'Vermelho',
+    rgb: '#FF0000'
+  },
+  {
+    nome: 'Azul',
+    rgb: '#0000FF'
+  },
+  {
+    nome: 'Amarelo',
+    rgb: '#FFFF00'
+  },
+  {
+    nome: 'Preto',
+    rgb: '#000000'
+  }
+]
+
+const cores = document.createElement('select')
+cores.multiple = true
+
+cor.forEach((cor) => {
+  const option = document.createElement('option')
+  option.value = cor.rgb
+  option.text = cor.nome
+
+  cores.appendChild(option)
+})
+
+console.log(cores);
+
+const form = document.querySelector('[data-js=form]')
+form.appendChild(cores)
+
+cores.addEventListener('change', (e) => {
+  const listaQuadrado = document.querySelector('[data-js=lista-quadrado]')
+  listaQuadrado.innerHTML = '';
+
+  [...e.target.options].map(item => {
+    if (item.selected) {
+      const quadrado = document.createElement('div')
+      quadrado.style.width = '100px'
+      quadrado.style.height = '100px'
+      quadrado.style.display = 'block'
+      quadrado.style.backgroundColor = item.value
+      quadrado.setAttribute('data-quadrado',"")
+
+      listaQuadrado.appendChild(quadrado)
+    }
+  });
+})
